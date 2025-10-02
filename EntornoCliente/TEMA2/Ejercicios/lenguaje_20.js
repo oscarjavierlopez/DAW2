68d96a664f42e96569c634d7ac804fec3e1b9489
oscarjavierlopez <oscarljavierlopez@gmail.com>
2025-10-02 21:08:05 +0200

    commit casa 02/10/2025

---
+++ EntornoCliente/TEMA2/Ejercicios/lenguaje_20.js
@@ -0,0 +1,10 @@
+//lenguaje_20.js - Operador ternario
+
+var resultado;
+
+//condición ? valor_si_true : valor_si_false;
+resultado=(true)?"Es verdadero":"Es falso";
+console.log(resultado);
+
+resultado=(4>7)?"mayor":"menor";
+console.log("4 es " + resultado + " que 7");
