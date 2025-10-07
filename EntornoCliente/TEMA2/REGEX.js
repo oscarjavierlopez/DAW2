4ba0497b6303a324e60e5d67987bf2c88e5a6fa3
oscarjavierlopez <oscarljavierlopez@gmail.com>
2025-10-07 10:07:14 +0200

    commit clase 07/10/2025

--- EntornoCliente/TEMA2/REGEX.js
+++ EntornoCliente/TEMA2/REGEX.js
@@ -2,9 +2,9 @@ const result = '--abcabcx4--'.match(/(abc){2}(.\d)/);
 console.log(result);
 /*
  [
-  'abcabcx4',   // [0] → Coincidencia completa con toda la expresión regular
-  'abc',        // [1] → Primer grupo capturado: `(abc)` → aunque se repite 2 veces, solo se guarda la última
-  'x4',         // [2] → Segundo grupo capturado: `(.\d)`
+  'abcabcx4',   // [0] → Coincidencia completa con toda la expresión regular → aunque se repita 2 veces, solo se guarda la primera
+  'abc',        // [1] → Primer grupo capturado: `(abc)` → aunque se repita 2 veces, solo se guarda la primera
+  'x4',         // [2] → Segundo grupo capturado: `(.\d)`→ aunque se repita 2 veces, solo se guarda la primera
   index: 2,     // Posición donde empieza la coincidencia dentro del string original
   input: '--abcabcx4--', // Cadena original sobre la que se aplicó `.match()`
   groups: undefined // No hay grupos con nombre definidos en la expresión regular
