f1bb17ce0371df15dd3df892b31651fda49bcfe2
oscarjavierlopez <oscarljavierlopez@gmail.com>
2025-10-21 13:37:33 +0200

    commit clase2 21/10/2025

--- EntornoCliente/PracticasEvaluables/practica1/LopezGomez_OscarJavir_Ejercicio2.js
+++ EntornoCliente/PracticasEvaluables/practica1/LopezGomez_OscarJavir_Ejercicio2.js
@@ -1,5 +1,7 @@
 let texto = "En un lugar de la mancha";
 let f4 = (texto) => texto.split(" ").filter((palabra) => palabra !== " ").join("");
-let f5 = (texto) => texto.split(" ").filter() / (texto.split(" ").length);
+let f5 = (texto) => texto.split(" ").join("").length / (texto.split(" ").length);
+let f6 = (texto) => texto.split(" ").filter((palabra) => /a|A/.test(palabra)).map((palabra) => palabra = palabra.length);;
 console.log(f4(texto));
-console.log("La media de longitud de las palabras es:" + f5(texto));
+console.log("La media de longitud de las palabras es: " + f5(texto));
+console.log(f6(texto));
