df09cee3352dd3bfc0651810c87cfe4d0ccb307c
oscarjavierlopez <oscarljavierlopez@gmail.com>
2025-10-21 16:44:34 +0200

    commit casa2 21/10/2025

--- EntornoCliente/PracticasEvaluables/practica1/LopezGomez_OscarJavir_Ejercicio2.js
+++ EntornoCliente/PracticasEvaluables/practica1/LopezGomez_OscarJavir_Ejercicio2.js
@@ -1,15 +1,10 @@
 let texto = "En un lugar de la mancha";
-let f4 = (texto) => texto.split(" ").join("");
+let f4 = (texto) => texto.split(" ").map((palabra) => palabra.charAt(0).toUpperCase() + palabra.slice(1)).join("");
 let f5 = (texto) => texto.split(" ").join("").length / (texto.split(" ").length);
 let f6 = (texto) => texto.split(" ").filter((palabra) => palabra.indexOf('a') !== -1).map((palabra) => palabra = palabra.length);;
 console.log(f4(texto));
 console.log("La media de longitud de las palabras es: " + f5(texto));
 console.log(f6(texto));
-console.log(texto.split("").forEach((caracter, indice, caracteres) => {
-    if(caracter == " " && (indice + 1) < caracteres.length){
-        caracteres[indice + 1] = caracteres[indice + 1].toUpperCase();
-    }
-    console.log(caracter);
-}));
+
 
 
