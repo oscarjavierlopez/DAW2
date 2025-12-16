d7ae521c18879b90e97c1146aad21dc004f433bc
oscarjavierlopez <oscarljavierlopez@gmail.com>
2025-12-16 07:43:47 +0100

    commit casa 16/12/2025

--- EntornoCliente/TEMA6/Ejercicios/PalabraOculta/js/ejercicio1.js
+++ EntornoCliente/TEMA6/Ejercicios/PalabraOculta/js/ejercicio1.js
@@ -5,8 +5,52 @@ const newWordButton = document.getElementById('nueva');
 const solutionButton = document.getElementById('solucion');
 const endButton = document.getElementById('finalizar');
 const result = document.getElementById('resultado');
+const dificultad = document.createElement('span');
+dificultad.style.border = '2px solid black';
+dificultad.style.padding = '5px';
+dificultad.innerHTML = 'dificultad';
+for (let i = 0; i < 3; i++) {
+    const label = document.createElement('label');
+    const button = document.createElement('input');
+    button.type = 'radio';
+    button.name = 'dificultad';
+    if (i === 0) {
+        label.innerText = '4 letras';
+        button.id = '4';
+        label.htmlFor = '4';
+        button.checked = true;
+    } else if (i === 1) {
+        label.innerText = '6 letras';
+        button.id = '6';
+        label.htmlFor = '6';
+    } else {
+        label.innerText = '8 letras';
+        button.id = '8';
+        label.htmlFor = '8';
+    }
+    dificultad.append(button);
+    dificultad.append(label);
+}
+document.querySelector('h3').after(dificultad);
+let correctWord = '';
 let totalPartidas = 0;
 let aciertos = 0;
+
+function generateNewWord() {
+    if (document.getElementById('4').checked === true) {
+        correctWord = palabras[0][Math.floor(Math.random() * palabras[0].length)];
+    } else if (document.getElementById('6').checked === true) {
+        correctWord = palabras[1][Math.floor(Math.random() * palabras[1].length)];
+    } else if (document.getElementById(8).checked === true) {
+        correctWord = palabras[2][Math.floor(Math.random() * palabras[2].length)];
+    }
+    letterInput.value = '';
+    disorderWord();
+    solutionButton.disabled = false;
+    newWordButton.disabled = true;
+    enteredWord.value = '';
+    result.style.display = 'none';
+}
 
 function disorderWord() {
     const lettersOfCorrectWord = [];
@@ -46,16 +90,6 @@ function enteredWordInput(e) {
     }
 }
 
-function generateNewWord() {
-    correctWord = palabras[Math.floor(Math.random() * palabras.length)];
-    letterInput.value = '';
-    disorderWord();
-    solutionButton.disabled = false;
-    newWordButton.disabled = true;
-    enteredWord.value = '';
-    result.style.display = 'none';
-}
-
 function viewSolution() {
     result.style.display = 'block';
     result.classList.remove('error');
@@ -67,7 +101,7 @@ function viewSolution() {
     totalPartidas++;
 }
 
-function endGame(){
+function endGame() {
     result.style.display = 'block';
     result.classList.remove('info');
     result.classList.remove('intentos');
@@ -78,18 +112,12 @@ function endGame(){
     newWordButton.disabled = true;
 }
 const palabras = [
-    "PROGRAMACION",
-    "SERVIDOR",
-    "BASE",
-    "DATOS",
-    "JAVASCRIPT",
-    "REACT",
-    "PYTHON",
-    "FRAMEWORK",
-    "APLICACION",
-    "DESARROLLO"
+    ["LUNA", "ROCA", "NUBE", "FARO", "CIMA", "RAMA", "PESO", "VIDA", "ALAS", "PURO"],
+    ["CAMINO", "VENTANA", "RUIDOS", "BRILLO", "SUEÑO", "ARENAS", "MONTES", "RIELES", "CIELOS", "BORDES"],
+    ["HORIZONTE", "MADEREROS", "CAMPANAS", "ESPEJADO", "LUMINOSA", "COSTURAS", "NAVEGADO", "SILENCIO",
+        "TERRAZAS", "MOTIVOSO"]
 ];
-let correctWord = '';
+
 generateNewWord();
 enteredWord.addEventListener('input', enteredWordInput);
 newWordButton.addEventListener('click', generateNewWord);
