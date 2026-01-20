4b2f292ca2856a3710252871491581e714c3ce21
oscarjavierlopez <oscarljavierlopez@gmail.com>
2026-01-20 12:10:21 +0100

    commit entorno servidor 20/01/2026

---
+++ EntornoServidor/P2_laravel_LopezGomez_OscarJavier/database/migrations/2026_01_20_101644_create_citas_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('citas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('usuario_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('cliente_id')->constrained('clientes')->onDelete('cascade');
+            $table->foreignId('servicio_id')->constrained('servicios')->onDelete('cascade');
+            $table->date('fecha');
+            $table->time('hora');
+            $table->string('estado');
+            $table->text('notas')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('citas');
+    }
+};
