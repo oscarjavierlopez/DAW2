97da8f50e8846b9d83218028961607920059549e
oscarjavierlopez <oscarljavierlopez@gmail.com>
2026-02-04 14:15:00 +0100

    cambios en django app

--- Despliegue/PROYECTO01/my_first_project/my_first_django_app/urls.py
+++ Despliegue/PROYECTO01/my_first_project/my_first_django_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name = "index"),
-    path('mi_primera_vista/', views.mi_primera_vista, name="Mi pimera vista"),
-    path('listar_personas/', views.listar_personas, name="listar personas")
+    path('mi_primera_vista/', views.mi_primera_vista, name="Mi_pimera_vista"),
+    path('listar_personas/', views.listar_personas, name="listar_personas"),
+    path('crear_persona/', views.crear_persona, name="crear_persona")
 ]
