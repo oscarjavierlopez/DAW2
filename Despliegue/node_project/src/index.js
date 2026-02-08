d390aa2de88fdafad52c091d4c1834ce2442b5e3
oscarjavierlopez <oscarljavierlopez@gmail.com>
2026-02-08 13:21:08 +0100

    commit despliegue 08/02/2026

--- Despliegue/node_project/src/index.js
+++ Despliegue/node_project/src/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use('/users', userRouter);
 
-const PORT = 3000;
+const PORT = process.env.PORT | 3000;
 
 app.listen(PORT, () => {
     console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
