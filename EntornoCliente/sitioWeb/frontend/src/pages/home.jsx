1b9a4d5e63820f7ec46ddd64e00522460d96e207
oscarjavierlopez <oscarljavierlopez@gmail.com>
2026-02-13 08:52:46 +0100

    feat: añadir busqueda por municipio

--- EntornoCliente/sitioWeb/frontend/src/pages/home.jsx
+++ EntornoCliente/sitioWeb/frontend/src/pages/home.jsx
@@ -8,6 +8,7 @@ import { EventCard } from "../components/eventCard";
 export function Home() {
     const { user, setUser } = useContext(userContext);
     const [eventos, setEventos] = useState([]);
+    const [search, setSearch] = useState('');
 
     useEffect(() => {
         fetch(`${BASE_URL}/eventos`)
@@ -15,14 +16,43 @@ export function Home() {
             .then(data => setEventos(data));
     }, []);
 
+    useEffect(() => {
+        const debounce = setTimeout(() => { 
+            fetch(`${BASE_URL}/eventos`)
+                .then(response => response.json())
+                .then(data => {
+                    if (/^\s*/.test(search)) {
+                        let regEx = new RegExp(`${search.toLowerCase()}.*`);
+                        let filteredEvents = data.filter((evento) => regEx.test(evento.municipio.nombre.toLowerCase()));
+                        setEventos(filteredEvents);
+                    } else {
+                        setEventos(data);
+                    }
+                });
+        }, 300);
+
+        return () => clearTimeout(debounce);
+    }, [search]);
+
+    const changeInput = (e) => {
+        setSearch(e.target.value);
+    }
+
     return (
         <>
             {user !== undefined ? <HeaderWithLogin /> : <HeaderWithoutLogin />}
 
             <div className="flex flex-col gap-6 justify-center items-center">
-                {eventos.map((evento) => {
+                <input type="search" value={search} className="bg-gray-600 block opacity-50 text-white w-4xl p-2 pl-2 
+            rounded-lg mb-6 text-sm" placeholder="Busca eventos en municipios cercanos..." onInput={changeInput} />
+
+                {eventos.length > 0 && eventos.map((evento) => {
                     return (<EventCard key={evento.id} nombre={evento.nombre} fecha={evento.fecha} imagen={evento.imagen} municipio={evento.municipio.nombre} />)
                 })}
+
+                {eventos.length === 0 && <p className="text-white font-bold">No hay resultados para la búsqueda</p>}
+
+                {(user !== undefined && user.rol === "administrador") && <button className="mb-3 p-4 rounded-sm text-sm text-white border border-white hover:scale-110">Añadir evento</button>}
             </div>
         </>
     );
